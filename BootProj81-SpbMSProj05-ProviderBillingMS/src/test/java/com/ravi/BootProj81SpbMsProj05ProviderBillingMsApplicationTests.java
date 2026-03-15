77db374e637682d4277aaedb9f30a9e8657841d1
Ravi kumar <rsravi433399@gmail.com>
2026-03-15 17:20:37 +0530

    Spb cloud MS project5 is added

---
+++ BootProj81-SpbMSProj05-ProviderBillingMS/src/test/java/com/ravi/BootProj81SpbMsProj05ProviderBillingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj81SpbMsProj05ProviderBillingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ BootProj82-SpbMSProj05-ConsumerCartMS-FeignClient/src/main/java/com/ravi/BootProj82SpbMsProj05ConsumerCartMsFeignClientApplication.java
@@ -0,0 +1,15 @@
+package com.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class BootProj82SpbMsProj05ConsumerCartMsFeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj82SpbMsProj05ConsumerCartMsFeignClientApplication.class, args);
+	}
+
+}
